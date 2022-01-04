76e1c24e5254a73bead6e5b2926a3942cb29c4ec
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-04 15:23:21 +0200

    added ConfigService

---
+++ Logger/Logger/Services/ConfigService.cs
@@ -0,0 +1,18 @@
+﻿using Logger.Models;
+using Logger.Providers.Abstractions;
+using Logger.Services.Abstractions;
+
+namespace Logger.Services
+{
+    public class ConfigService : IConfigService
+    {
+        private readonly IConfigProvider _configProvider;
+
+        public ConfigService(IConfigProvider configProvider)
+        {
+            _configProvider = configProvider;
+        }
+
+        public LoggerConfig LoggerConfig => _configProvider.Config.LoggerConfig;
+    }
+}
