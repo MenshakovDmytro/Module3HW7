fb13b2afdf17bc8386856d597361c4da29c2d771
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-04 15:22:24 +0200

    added IFileConversionService

---
+++ Logger/Logger/Services/Abstractions/IFileConversionService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Logger.Models;
+
+namespace Logger.Services.Abstractions
+{
+    public interface IFileConversionService
+    {
+        public Task<Config> ConvertJsonToConfig(string jsonConfig);
+    }
+}
