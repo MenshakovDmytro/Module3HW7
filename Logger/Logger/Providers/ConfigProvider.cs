b17f79244b764dc75eb1173e97913554b67c432a
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-04 15:21:48 +0200

    added ConfigProvider

---
+++ Logger/Logger/Providers/ConfigProvider.cs
@@ -0,0 +1,27 @@
+﻿using Logger.Models;
+using Logger.Providers.Abstractions;
+using Logger.Services.Abstractions;
+
+namespace Logger.Providers
+{
+    public class ConfigProvider : IConfigProvider
+    {
+        private const string ConfigFile = "config.json";
+
+        private readonly Config _config;
+        private readonly IFileConversionService _fileConversionService;
+
+        public ConfigProvider(IFileConversionService fileConversionService)
+        {
+            _fileConversionService = fileConversionService;
+            _config = LoadConfig();
+        }
+
+        public Config Config => _config;
+
+        private Config LoadConfig()
+        {
+            return _fileConversionService.ConvertJsonToConfig(ConfigFile).GetAwaiter().GetResult();
+        }
+    }
+}
