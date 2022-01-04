55e4fe05e77032ed80913ff2493e14e18a27d8a5
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-04 15:22:57 +0200

    added ActionsService

---
+++ Logger/Logger/Services/ActionsService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Logger.Helper;
+using Logger.Models;
+using Logger.Services.Abstractions;
+
+namespace Logger.Services
+{
+    public class ActionsService : IActionsService
+    {
+        private readonly ILoggerService _logger;
+
+        public ActionsService(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+
+        public bool WriteInfoLog()
+        {
+            _logger.LogAsync(LogType.Info, $"Start method: {nameof(WriteInfoLog)}");
+            return true;
+        }
+
+        public bool SkipLogic()
+        {
+            throw new BusinessException("Skipped logic in method");
+        }
+
+        public bool BreakLogic()
+        {
+            throw new Exception("I broke a logic");
+        }
+    }
+}
