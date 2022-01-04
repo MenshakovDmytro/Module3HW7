be178259026755029c947bbbba867be6df432309
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-04 15:22:36 +0200

    added ILoggerFileService

---
+++ Logger/Logger/Services/Abstractions/ILoggerFileService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Logger.Services.Abstractions
+{
+    public interface ILoggerFileService : IDisposable
+    {
+        public Task WriteAsync(string data);
+        public Task WriteBackupAsync(string data);
+        public void CreateDirectory(string directoryName);
+        public void CreateFile(string fileName);
+    }
+}
